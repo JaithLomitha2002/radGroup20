65553c842b8c10d2c958374646dc23b3fc17e6c9
Your Name <2022cs046@stu.ucsc.cmb.ac.lk>
2024-09-01 10:36:25 +0530

    Initial commit or your commit message

---
+++ .history/backend/src/routes/hotels_20240831072704.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response} from "express";
+import Hotel from "../models/hotel";
+
+const router = express.Router();
+
+router.get("/search", async (req: Request, res: Response) => {
+    try{
+        const hotels = await Hotel.find();
+
+    }catch(error){
+        console.log("error", error);
+        res.status(500).json({message:"Something went wrong"});
+
+    }
+});
---
+++ .history/backend/src/routes/hotels_20240831132258.ts
@@ -0,0 +1,21 @@
+import express, {Request, Response} from "express";
+import Hotel from "../models/hotel";
+
+const router = express.Router();
+
+router.get("/search", async (req: Request, res: Response) => {
+    try{
+        const pageSize = 5;
+        const pageNumber = parseInt(
+            req.query.page ? req.query.page.toString() : "1"
+        );
+        //pageNumber = 3 
+        const skip = (pageNumber - 1) * pageSize;
+        const hotels = await Hotel.find().skip(skip).limit(pageSize);
+
+    }catch(error){
+        console.log("error", error);
+        res.status(500).json({message:"Something went wrong"});
+
+    }
+});
